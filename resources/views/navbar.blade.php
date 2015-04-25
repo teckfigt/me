e28cef78ba5d652788e4e2d4c3bfae96eb21f2db
aa-ta_suzuki <ta_suzuki@corp.allabout.co.jp>
2015-04-26 06:30:20 +0900

    余分なファイル削除と、画面共通要素を分ける

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,3 @@
-@extends('apphead')
-
-@section('title', '好き登録')
-
     <body role="document">
 
       <!-- Fixed navbar -->
@@ -37,28 +33,3 @@
           </div><!--/.nav-collapse -->
         </div>
       </nav>
-
-
-      <div class="container theme-showcase" role="main">
-        <!-- Main jumbotron for a primary marketing message or call to action -->
-        <div class="jumbotron">
-
-
-          <div class="page-header">
-            <h1>好きを登録</h1>
-          </div>
-          <form action="" method="get">
-            <div class="form-group">
-              <label for="pass">すき</label>
-              <input type="text" class="form-control" name="pass" placeholder="すきを入力">
-            </div>
-            <div class="form-group">
-              <label for="memo">メモ</label>
-              <textarea class="form-control" name="memo" rows="5" placeholder="メモを入力"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">登録する</button>
-          </form>
-
-        </div>
-
-@extends('endsection')
