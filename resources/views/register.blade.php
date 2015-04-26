0b8bcfc294fe4f0f0fe7494f88d540d3d7c9a5fa
aa-ta_suzuki <ta_suzuki@corp.allabout.co.jp>
2015-04-27 07:19:39 +0900

    JavaScript周りの実装

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,6 +10,7 @@
 
     <div class="page-header">
       <h2>すきを登録</h2>
+      <p>あなたの好きを入力してください、もの、こと、何でも大丈夫です</p>
     </div>
 
     <div class="form-group">
@@ -21,11 +22,17 @@
       <textarea class="form-control" name="memo" rows="5" id="memotext" placeholder="メモを入力"></textarea>
     </div>
     <button class="btn btn-primary" id="btn_register" onclick="register()">登録する</button>
+    <button class="btn btn-primary" id="btn_to_list">リストを見る</button>
 
       <script>
       function register() {
-        like_text = document.getElementById("likeword").value;
-        memo_text = document.getElementById("memotext").value;
+
+        var like_text = document.getElementById("likeword").value;
+        var memo_text = document.getElementById("memotext").value;
+
+        /*var data = {}*/
+        localStorage.setItem("word", like_text);
+        localStorage.setItem("memo", memo_text);
         window.alert(like_text + "\n" + memo_text);
       }
       </script>
