df8735fb56107978f686f5471650f421dba8e215
aa-ta_suzuki <ta_suzuki@corp.allabout.co.jp>
2015-04-26 18:09:56 +0900

    registerの変更

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,27 +1,32 @@
 @extends('appmaster')
 
-@section('title', '好き登録')
+@section('title', 'すき登録')
 
 @section('content')
-      <div class="container theme-showcase" role="main">
-        <!-- Main jumbotron for a primary marketing message or call to action -->
-        <div class="jumbotron">
+<div class="container theme-showcase" role="main">
+  <!-- Main jumbotron for a primary marketing message or call to action -->
+  <div class="jumbotron">
 
 
-          <div class="page-header">
-            <h2>すきを登録</h2>
-          </div>
-          <form action="likelist" method="get">
-            <div class="form-group">
-              <label for="pass">すき</label>
-              <input type="text" class="form-control" name="pass" placeholder="すきを入力">
-            </div>
-            <div class="form-group">
-              <label for="memo">メモ</label>
-              <textarea class="form-control" name="memo" rows="5" placeholder="メモを入力"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">登録する</button>
-          </form>
+    <div class="page-header">
+      <h2>すきを登録</h2>
+    </div>
 
-        </div>
-@stop
+    <div class="form-group">
+      <label for="like">すき</label>
+      <input type="text" class="form-control" name="like" id="likeword" placeholder="すきを入力">
+    </div>
+    <div class="form-group">
+      <label for="memo">メモ</label>
+      <textarea class="form-control" name="memo" rows="5" id="memotext" placeholder="メモを入力"></textarea>
+    </div>
+    <button class="btn btn-primary" id="btn_register" onclick="register()">登録する</button>
+
+      <script>
+      function register() {
+        like_text = document.getElementById("likeword").value;
+        memo_text = document.getElementById("memotext").value;
+        window.alert(like_text + "\n" + memo_text);
+      }
+      </script>
+      @stop
