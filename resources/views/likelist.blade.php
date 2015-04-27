560c74d6057ece81831dc5ee28e8c8380c50a5ae
aa-ta_suzuki <ta_suzuki@corp.allabout.co.jp>
2015-04-27 11:26:36 +0900

    ロゴの追加、ナビゲーションバーの修正

--- resources/views/likelist.blade.php
+++ resources/views/likelist.blade.php
@@ -5,12 +5,21 @@
 @section('content')
 <div class="container theme-showcase" role="main">
 <ul class="list-group"  ondragover="f_dragover(event)" ondrop="f_drop(event)">
-    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item1">商品1</li>
-    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item2">商品2</li>
-    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item3">商品3</li>
+    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item1">家族</li>
+    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item2">写真</li>
+    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item3">お金</li>
+    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item4">聞かれること</li>
+    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item5">頼りにされること</li>
+    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item6">ライブにいくこと</li>
+    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item7">話をすること</li>
+    <li class="list-group-item" draggable="true" ondragstart="f_dragstart(event)" id="item8">電話すること</li>
 </ul>
 <div class="jumbotron drop-destination" id="dropbox" ondragover="f_dragover(event)" ondrop="f_drop(event)">
 	</div>
+  <div class="jumbotron drop-destination" id="dropbox" ondragover="f_dragover(event)" ondrop="f_drop(event)">
+  </div>
+  <div class="jumbotron drop-destination" id="dropbox" ondragover="f_dragover(event)" ondrop="f_drop(event)">
+  </div>
 </div>
 
 <script>
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -8,25 +8,12 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">me logo</a>
+            <a class="navbar-brand" href="/"><img src="{{ asset('img/me_logo.png') }}" height="25px"></a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
+              <li class="active"><a href="menu">Menu</a></li>
+              <li><a href="how_to.html">How To</a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div>
